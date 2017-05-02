80d261cdf72e5e538ce2c4aed7268b977c7827b0
josecoll <jose.coll@r3cev.com>
2017-05-02 19:01:15 +0100

    Updates to address MH latest set of review comments.

--- node/src/main/kotlin/net/corda/node/internal/CordaRPCOpsImpl.kt
+++ node/src/main/kotlin/net/corda/node/internal/CordaRPCOpsImpl.kt
@@ -66,7 +66,7 @@ class CordaRPCOpsImpl(
     @RPCReturnsObservables
     override fun <T : ContractState> vaultServiceTrackBy(criteria: QueryCriteria,
                                                          paging: PageSpecification,
-                                                         sorting: Sort): Vault.QueryResults<T> {
+                                                         sorting: Sort): Vault.PageAndUpdates<T> {
         return database.transaction {
             services.vaultService.trackBy<T>(criteria, paging, sorting)
         }
